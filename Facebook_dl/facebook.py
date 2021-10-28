19b587ecc38ffb5b85f5372184e2bb02b29c9154
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2021-10-28 15:41:19 +0600

    added facebook_dl

---
+++ Facebook_dl/facebook.py
@@ -0,0 +1,58 @@
+import re
+import requests
+import urllib.request
+
+
+class FaceBook:
+    ''' A class to handle facebook video download requests from OAV Downloder. '''
+
+    def __init__(self, link):
+        ''' Initializing the FaceBook class Instance with video url. '''
+
+        # The video url
+        self.link = link
+
+        # Creating a custom title for the video.
+        self.auth, self.video_id = link.split('/')[3], link.split('/')[5]
+        self.name = f"{self.auth}-{self.video_id}"
+
+    def download_url(self):
+        ''' This function returns a online stream url for the video and 
+        it can be manually download from there. '''
+
+        # The html data retrived from  the url.
+        html = requests.get(self.link)  
+
+        # trying to get the downloadable url for (hd/sd)video from the html data.
+        try:
+            # downloadable url for HD video.
+            url = re.search('hd_src:"(.+?)"',html.text)[1]
+        except:
+            # downloadable url for SD video.
+            url = re.search('sd_src:"(.+?)"',html.text)[1]
+
+        return url
+    
+    def download(self):
+        ''' The download function is responsible for downloading the video 
+        by calling the download_url function. '''
+        try:
+            
+            # doenloads the video from the url from download_url method.
+            urllib.request.urlretrieve(self.download_url(), f"{self.name}.mp4")
+            
+        except (ConnectionResetError) as e:
+            print('Slow Internet Connection, please check your Internet Connection!')
+        except:
+            print('Slow Internet Connection, please check your Internet Connection!')
+        
+
+
+#!!! TEST CODE BLOCK !!!
+if __name__ == "__main__":
+    facebook_dl = FaceBook(link=input("Video url: "))
+    facebook_dl.download()
+
+
+
+
--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # General-Python-Useages
 Contents several Python mini projects which are beginner friendly and fun to make and use.
 
-## GIF-MAKER
+### GIF-MAKER
 It is a python script to create gifs from any format of Images. It usage's pillow 8.3.2, os, logging, glob.
 
-## DataStructures
+### DataStructures
 I am learning data-Structurs using python and i will upload the python implementation in this directory.
 
-## Pyshare
+### Pyshare
 It is a script using python http.server module in backend and there is a .exe of this made by pyinstaller.
 
-## YouFast-Downloader
+### Facebook_dl
+A simple python script for downloading facebook videos for free (:
+
+### YouFast-Downloader
 A simple downloader for youtube videos  using --  pytube, plyer, kivy, kivymd
