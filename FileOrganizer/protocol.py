1b2a9c1a33a9490e4f0af643981541a1ab619acf
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2023-03-26 11:53:12 +0600

    Update protocol.py

--- FileOrganizer/protocol.py
+++ FileOrganizer/protocol.py
@@ -1,7 +1,8 @@
 import os
 import re
+import shutil
 from settings import config
-from pathlib import Path, PurePath
+from pathlib import Path, PurePath, PosixPath
 
 class PROTOCOL:
     __instance : "PROTOCOL"  = None
@@ -12,48 +13,55 @@ class PROTOCOL:
         
         return cls.__instance
 
-    def copyFile(self : "PROTOCOL", dstPath : Path, currentPath : Path, fname : str) -> None:
-        pass
+    def is_organized(self : "PROTOCOL", root : PosixPath, fname : str) -> bool:
+        return str(root).split('/')[-1] == fname.split('.')[-1].upper()
+    
+    def handleFile(self: "PROTOCOL", dstPath: Path, currentPath: Path, fname: str, package: "re.Match" = None) -> None:
+        if package:
+            sub_package_separator = config.get_indicators()["sub_package"]
+            sub_packages = package.group(1).split(sub_package_separator)
+            sub_dir = dstPath.parent
 
-    def moveFile(self : "PROTOCOL", dstPath : Path, currentPath : Path, fname : str) -> None:
-        pass
+            for sub_package in sub_packages:
+                sub_dir = sub_dir / sub_package
+                sub_dir.mkdir(exist_ok=True)
+
+            new_fname = sub_dir / fname.replace(package.group(0), "")
+        else:
+            new_fname = dstPath / fname
+
+        shutil.copy2(PurePath.joinpath(currentPath, fname), new_fname)
+
+        if config.cut_mode():
+            os.remove(PurePath.joinpath(currentPath, fname))
 
     def organize(self : "PROTOCOL", dpath : Path) -> None:
         package_list : list[str] = []
         catagory_list : list[str] = []
-        sub_dir_list : list[Path] = []
         indicators : dict[str, str] = config.get_indicators()
-        
+
         for root, dirs, files in os.walk(dpath):
             root = Path(root)
             for _file in files:
-                has_package : bool = False
-                if root == dpath:
-                    package = re.search(f"{indicators['package']}(.*?){indicators['package']}", _file)
+                if not self.is_organized(root, _file):
+                    has_package : bool = False
+                    if root == dpath:
+                        package = re.search(f"{indicators['package']}(.*?){indicators['package']}", _file)
+                        if package:
+                            if package.group(1) not in package_list:
+                                package_list.append(package.group(1))
+                            has_package = True
 
-                    if package:
-                        if package.group(1) not in package_list:
-                            package_list.append(package.group(1))
-                            PurePath.joinpath(root, package.group(1)).mkdir(exist_ok=True)
-                        has_package = True
+                        if _file.split('.')[-1].upper() not in catagory_list and not has_package:
+                            catagory_list.append(_file.split('.')[-1].upper())
+                            PurePath.joinpath(root, catagory_list[-1]).mkdir(exist_ok=True)
 
-                    if _file.split('.')[-1].upper() not in catagory_list and not has_package:
-                        catagory_list.append(_file.split('.')[-1].upper())
-                        PurePath.joinpath(root, catagory_list[-1]).mkdir(exist_ok=True)
+                        dstPath = PurePath.joinpath(root, _file.split('.')[-1].upper() if not has_package else package.group(1))
+                        self.handleFile(dstPath, root, _file, package)
 
-                    dstPath = PurePath.joinpath(root, _file.split('.')[-1].upper() if not has_package else package.group(1))
-                    self.copyFile(dstPath, root, _file) if not config.cut_mode() else self.moveFile(dstPath, root, _file)
-                else:
-                    sub_dir_list.append(root)
-
-        print(catagory_list, package_list)
-
-        for subDir in sub_dir_list:
-            self.organize(subDir)
-
-        for package in package_list:
-            self.organize(package)
-            
 
 system = PROTOCOL()
-system.organize(Path.cwd())
+
+if __name__ == "__main__":
+    test_path = "/home/eaegon/Documents/GITHUB/General-Python-Useages/FileOrganizer/TEST"
+    system.organize(PurePath(test_path))
